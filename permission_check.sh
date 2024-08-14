db8a43de0375ec2ba8e166f9b8664004bb8d4aed
Dolma <dolmatgurung1@gmail.com>
2024-08-14 18:14:43 -0400

    Read and execute check

--- permission_check.sh
+++ permission_check.sh
@@ -15,4 +15,30 @@ else
 fi
 }
 
+read(){
+
+FILE="$1"
+if [ -r "$FILE" ] 
+then
+   echo "Readable"
+else
+   echo "Not Readble"
+fi
+}
+
+execute(){
+
+FILE="$1"
+if [ -x "$FILE" ] 
+then
+   echo "Executable"
+else
+   echo "Not Executable"
+fi
+
+}
+
+
 write "script.sh"
+read "script.sh"
+execute "script.sh"
