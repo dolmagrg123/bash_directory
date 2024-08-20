36fd0e372ea82563f4d17773d6ec9b2053000ba5
Dolma <dolmatgurung1@gmail.com>
2024-08-20 01:40:15 -0400

    private address script updated

--- System_Info.sh
+++ System_Info.sh
@@ -48,7 +48,8 @@ menu() {
 }
 
 ip_address(){
-    ip addr show $(ip route | awk '/default/ { print $5 }') | grep "inet" | head -n 1 | awk '/inet/ {print $2}' | cut -d'/' -f1
+    IP="hostname -I"
+   echo "Your IP address is $IP"
 }
 current_user(){
 
