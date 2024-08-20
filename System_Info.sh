9a53cbe6a868c3393d891fa48b3440afa9c3d45c
Dolma <dolmatgurung1@gmail.com>
2024-08-20 00:03:03 -0400

    System Info Script Requirements Added

---
+++ System_Info.sh
@@ -0,0 +1,21 @@
+# As an Entry Level DevOps Engineer, you are tasked with creating a user-friendly script that can be run from the terminal for a user to easily get important system information.
+
+# Create a script that will present the following menu to a user to choose from:
+
+# 1. IP Addresses #ex answer: "Your private IP is 172.31.1.150, and you public IP is 3.235.129.224."
+# 2. Current User #ex answer: "You are ubuntu."
+# 3. CPU Information #ex answer: "The system has 1 CPU." 
+# 4. Memory Information #ex answer "There is 330 Mebibyte unused memory of total 957 Mebibyte."
+# 5. Top 5 Memory Processes #This can be a table that's produced by a command, ex answer:
+
+# "pmem   pid       cmd 
+#  2.9        8047    /usr/lib/snapd/snapd
+#  2.7        181      /sbin/multipathd -d -s
+#  2.3        664      /usr/bin/python3 /usr/share/unattended-upgrades/unattended-upgrade-shutdown --wait-for-signal
+#  2.1        516      /usr/bin/python3 /usr/bin/networkd-dispatcher --run-startup-triggers
+#  1.6        524      /snap/amazon-ssm-agent/7993/amazon-ssm-agent"
+
+# 6. Top 5 CPU Processes #Same as above
+# 7. Network Connectivity #ask user for a website or IP address to connect to first and then produce a statement regarding the network connection, ex answer: "It took 7ms to connect to www.google.com and there was 0% data packet loss."
+
+# The script should continue to ask the user to choose from the menu until they want to exit.
