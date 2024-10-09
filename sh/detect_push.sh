8e899fa207cdbc6b3cde546daeb3fb5b2236bdfb
Dolma <dolmatgurung1@gmail.com>
2024-10-09 19:13:01 -0400

    vs code

---
+++ sh/detect_push.sh
@@ -0,0 +1,7 @@
+# Write a script that will detect new files made in a specific directory, 
+# and push the changes to the repository in GitHub. 
+# If the repository doesn't exist in GitHub, ask the user if they'd like to create one 
+# and then push to GitHub.
+
+
+
